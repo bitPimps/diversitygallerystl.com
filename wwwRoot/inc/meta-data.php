b2671d8bfa9d85942be35cdad2c104d5ed572656
bitPimps <bitpimps@gmail.com>
2019-06-03 16:20:21 -0500

    Performance updates
    Adding client-side caching, minifying CSS

--- wwwRoot/inc/meta-data.php
+++ wwwRoot/inc/meta-data.php
@@ -17,7 +17,7 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin|Lato" />
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-		<link rel="stylesheet" href="/css/main.css" />
+		<link rel="stylesheet" href="/css/main.min.css" />
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>

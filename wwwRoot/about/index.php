3128c402f340290860ce5f68f901c30338e68b63
bitPimps <bitpimps@gmail.com>
2020-01-10 12:55:13 -0600

    Update index.php
    Updated age of kids

--- wwwRoot/about/index.php
+++ wwwRoot/about/index.php
@@ -22,7 +22,7 @@
 					</h1>
 					<div class="row">
 						<div class="col-md-7">
-							<p>Leslie was born and raised in St. Louis, where she currently lives with her husband AJ and youngest daughter, Skylar (8). Her family also includes two more daughters, Kamille (29) and Ashley (28). Before beginning her entrepreneurial journey in 2000, Leslie worked in healthcare administration and became a licensed Real Estate Agent.</p>
+							<p>Leslie was born and raised in St. Louis, where she currently lives with her husband AJ and youngest daughter, Skylar (10). Her family also includes two more daughters, Kamille (30) and Ashley (29). Before beginning her entrepreneurial journey in 2000, Leslie worked in healthcare administration and became a licensed Real Estate Agent.</p>
 							<p>Since opening Diversity Gallery, Leslie has been very active within her community, consulting other local businesses and performing leadership roles. She wears many hats, including Natural Hair Stylist, Fashion and Jewelry Designer, Event Planner, Motivational Speaker and Lifestyle Coach.</p>
 							<p>Leslie is grateful for her family, friends, and community for their unwavering support of Diversity Gallery. It's because of them that Diversity Gallery has continued to thrive for almost two decades.</p>
 						</div>

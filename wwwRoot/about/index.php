ff743afa3d43a095de9b036065336d149dc6e5af
bitPimps <bitpimps@gmail.com>
2021-01-28 14:32:33 -0600

    Add STL Made and Testimonials
    Added STL Made video and link.
    Added Testimonials.

--- wwwRoot/about/index.php
+++ wwwRoot/about/index.php
@@ -16,7 +16,7 @@
 		</div><!-- /jumbotron -->
 		<main role="main">
 			<div class="band">
-				<div class="container">
+				<div class="container pb-5">
 					<h1 class="text-uppercase heading-1">
 						<span>About Leslie Christian-Wilson</span>
 					</h1>
@@ -25,6 +25,22 @@
 							<p>Leslie was born and raised in St. Louis, where she currently lives with her husband AJ and youngest daughter, Skylar (10). Her family also includes two more daughters, Kamille (30) and Ashley (29). Before beginning her entrepreneurial journey in 2000, Leslie worked in healthcare administration and became a licensed Real Estate Agent.</p>
 							<p>Since opening Diversity Gallery, Leslie has been very active within her community, consulting other local businesses and performing leadership roles. She wears many hats, including Natural Hair Stylist, Fashion and Jewelry Designer, Event Planner, Motivational Speaker and Lifestyle Coach.</p>
 							<p>Leslie is grateful for her family, friends, and community for their unwavering support of Diversity Gallery. It's because of them that Diversity Gallery has continued to thrive for almost two decades.</p>
+							<hr>
+							<h2>St. Louis Made Feature</h2>
+							<p>Read the <a href="https://thestl.com/community/empowering-the-community/" target="_blank"><em>Empowering the Community</em></a> article featuring Leslie!</p>
+							<div class="embed-responsive embed-responsive-16by9">
+								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Zps-NnZlzoA?rel=0" allowfullscreen></iframe>
+							</div>
+							<hr>
+							<h2>Testimonials</h2>
+							<blockquote>
+								<p>In 2006, I had the opportunity to study abroad in London, England -along with my Washington University classmates. I remember going to the Delmar Loop to find some earrings to take with me. As I strolled along the Boulevard, I spotted the most enchanting shop called "Diversity Gallery."  When I entered, it felt like I had been translated to another amazingly beautiful dimension!! The cozy space was jammed-packed with unique African and African American artwork, tapestries, beauty products, fragrances, and jewelry! I was so amazed and delighted! Then, a beautiful, charming woman emerged from the back of the shop, turned on some improvisational jazz, and burned some heady incense. She had these perfectly coifed honey brown locks that she wore on the top of her head-I knew I was at home. "Welcome to Diversity Gallery, I'm Leslie, may I help you?" I was speechless-at first-because she was so regal and confident in who she was. She was everything I wanted to be. I explained what I wanted and where I was going, and we began to talk about everything. By the time I left, she had transferred some of her confidence to me and I have never been the same. Today, she is my mentor and still pours blessings into my life. If you are looking for an experience that will change your life, then step into Diversity Gallery, it will be the best decision you've ever made.</p>
+								<footer>Myah Maedell Singh</footer>
+							</blockquote>
+							<blockquote>
+								<p>Hey Leslie just wanted to let you know how much I appreciate you. From the first day I came into your shop you never sold me anything that I didn't need. You have always been that person that keeps it 💯 and I will always love you for that. I'm  on a new hair journey in my life and I look forward to your guidance and expertise. Thanks again for everything!</p>
+								<footer>Vanessa Whitfield</footer>
+							</blockquote>
 						</div>
 						<div class="col-md-5">
 							<div class="thumbnail">
